03c21f595154fb4d2da058f1bc3648c54ea4ad1a
Josh Freeman <josh@viion.co.uk>
2018-11-17 15:19:05 +0000

    Add secondary data to companion enrich response

---
+++ src/Service/Content/GameData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service\Content;
+
+use App\Service\Redis\Cache;
+
+class GameData
+{
+    /** @var Cache */
+    private static $cache = null;
+    
+    /**
+     * Initialize cache
+     */
+    public static function init()
+    {
+        if (!self::$cache === null) {
+            self::$cache = new Cache();
+        }
+    }
+    
+    /**
+     * get a single piece of content from the cache
+     */
+    public static function one($contentName, $contentId)
+    {
+        self::init();
+        
+        $content   = self::$cache->get("xiv_{$contentName}_{$contentId}");
+        $secondary = self::$cache->get("xiv2_{$contentName}_{$contentId}") ?: [];
+    
+        if (!$content) {
+            throw new \Exception("Game Data does not exist: {$contentName} {$contentId}");
+        }
+        
+        // merge main and secondary content
+        return (Object)array_merge(
+            (array)$content,
+            (array)$secondary
+        );
+    }
+}
