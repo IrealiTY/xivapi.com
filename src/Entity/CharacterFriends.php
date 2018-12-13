1dfdef99f14535a228f44ebf92e2438c4817d2f3
Josh Freeman <josh.freeman@sykescottages.co.uk>
2018-12-13 14:53:11 +0000

    Fix all column names in db indexes

--- src/Entity/CharacterFriends.php
+++ src/Entity/CharacterFriends.php
@@ -11,8 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
  *          @ORM\Index(name="state", columns={"state"}),
  *          @ORM\Index(name="updated", columns={"updated"}),
  *          @ORM\Index(name="priority", columns={"priority"}),
- *          @ORM\Index(name="notFoundChecks", columns={"notFoundChecks"}),
- *          @ORM\Index(name="achievementsPrivateChecks", columns={"achievementsPrivateChecks"})
+ *          @ORM\Index(name="notFoundChecks", columns={"not_found_checks"}),
+ *          @ORM\Index(name="achievementsPrivateChecks", columns={"achievements_private_checks"})
  *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CharacterFriendsRepository")
