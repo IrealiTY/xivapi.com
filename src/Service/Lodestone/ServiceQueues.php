63930a23f8a00e5079f9d51024808192594744c5
Josh Freeman <josh.freeman@sykescottages.co.uk>
2018-12-18 11:37:55 +0000

    Tweak queue values

--- src/Service/Lodestone/ServiceQueues.php
+++ src/Service/Lodestone/ServiceQueues.php
@@ -16,10 +16,10 @@ class ServiceQueues
     const UPDATE_TIMEOUT = 86400;
     
     // maximum characters to process per minute
-    const TOTAL_ACHIEVEMENT_UPDATES  = 15; // 10 pages, take roughly 4 seconds
-    const TOTAL_PVP_TEAM_UPDATES     = 50; // Usually only 1 page
-    const TOTAL_LINKSHELL_UPDATES    = 40; // Max: 128 = 3 pages, 40 @ 1.5s a LS
-    const TOTAL_FREE_COMPANY_UPDATES = 30;
-    const TOTAL_CHARACTER_UPDATES    = 100;
-    const TOTAL_CHARACTER_FRIENDS    = 50;
+    const TOTAL_ACHIEVEMENT_UPDATES  = 15;  // Avg: 4.0s - Always 10 pages
+    const TOTAL_PVP_TEAM_UPDATES     = 50;  // Avg: 0.4s - Usually only 1 page
+    const TOTAL_LINKSHELL_UPDATES    = 40;  // Avg: 0.4s - Max 3 pages
+    const TOTAL_FREE_COMPANY_UPDATES = 15;  // Avg: 1s   - Max: 10 pages
+    const TOTAL_CHARACTER_UPDATES    = 80;  // Avg: 0.4s - Max 1 page
+    const TOTAL_CHARACTER_FRIENDS    = 50;  // Avg: 0.4s - Max 4 pages
 }
